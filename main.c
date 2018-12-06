e7f4c2503635cb2fcb6e7293eb816678b67045ad
xjancek <43437618+xjancek@users.noreply.github.com>
2018-12-06 13:09:27 +0100

    Funkcia_n (vytvorenie spájaného zoznamu a načítanie záznamov zo súboru)

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct zaznam {
     char kategoria[51];
@@ -29,6 +30,49 @@ void n(struct zaznam **zaznam_zac, struct zaznam **zaznam_act, int *poc_zaznamov
             poc_zaznam++;}
     }
     *poc_zaznamov = poc_zaznam;
+
+    rewind(f);
+
+    *zaznam_zac = NULL;
+
+    *zaznam_zac = malloc(sizeof(struct zaznam));
+    if (*zaznam_zac == NULL)
+        printf("something is wrong\n");
+    *zaznam_act = *zaznam_zac;
+
+    while (!feof(f)){
+
+        if (((*zaznam_act)->next = malloc(sizeof(struct zaznam))) == NULL) {
+            printf("Malo pamate.\n");
+            return;
+        }
+
+        fscanf(f, "%*c");
+        fscanf(f, "%*c");
+        fscanf(f, "%51[^\n]", (*zaznam_act)->kategoria);
+        fscanf(f, "%*c");
+        fscanf(f, "%51[^\n]", (*zaznam_act)->znacka);
+        fscanf(f, "%*c");
+        fscanf(f, "%101[^\n]", (*zaznam_act)->predajca);
+        fscanf(f, "%*c");
+        fscanf(f, "%10[^\n]", (*zaznam_act)->cena);
+        fscanf(f, "%*c");
+        fscanf(f, "%10[^\n]", (*zaznam_act)->rok_vyroby);
+        fscanf(f, "%*c");
+        fscanf(f, "%201[^\n]", (*zaznam_act)->stav_vozidla);
+        fscanf(f, "%*c");
+
+
+        *zaznam_act = (*zaznam_act)->next;
+    }
+    (*zaznam_act)->next = NULL;
+
+    if (*poc_zaznamov == 0)
+        printf("Zaznamy neboli nacitane\n");
+    else
+        printf("Nacitalo sa %d zaznamov\n", *poc_zaznamov);
+
+    fclose(f);
 
 }
 //---------------------------------------------------------------------
